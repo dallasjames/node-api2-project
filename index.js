1fe65a5d02b9358c08f232d6b36ea4a9f0b4120c
Dallas James <dallasjames42@gmail.com>
2019-12-06 19:20:42 -0700

    posts are done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.get("/api/posts", (req, res) => {
         })
         .catch(err => {
             res.status(500).json({
-                error: "Sum Ting Wong"
+                error: "The posts information could not be retrieved."
             })
         })
 })
@@ -23,11 +23,15 @@ server.get("/api/posts/:id", (req, res) => {
             if (post) {
                 res.status(200).json(post)
             } else {
-                res.status(404).json({ message: "It Not Here" })
+                res.status(404).json({ 
+                    message: "The post with the specified ID does not exist."
+                 })
             }
         })
         .catch(err => {
-            res.status(500).json({ error: "I Screw Up" })
+            res.status(500).json({ 
+                error: "The post information could not be retrieved." 
+            })
         })
 })
 
@@ -37,15 +41,70 @@ server.get("/api/posts/:id/comments", (req, res) => {
             if (com) {
                 res.status(200).json(com)
             } else {
-                res.status(404).json({ message: "No Comment Here" })
+                res.status(404).json({ 
+                    message: "The post with the specified ID does not exist."
+                 })
             }
         })
         .catch(err => {
-            res.status(500).json({ error: "Not Yet Fixed" })
+            res.status(500).json({ 
+                error: "The comments information could not be retrieved."
+             })
         })
 })
 
+server.post("/api/posts", (req, res) => {
+    const post = {
+        title: req.body.title,
+        contents: req.body.contents
+    }
+    if (!post.title || !post.contents) {
+        return res.status(400).json({ 
+            errorMessage: "Please provide title and contents for the post." 
+        })
+    }
+    db.insert(post)
+        .then(newPost => {
+            res.status(201).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({ 
+                error: "There was an error while saving the post to the database" 
+            })
+        })
+})
 
+server.post("/api/posts/:id/comments", (req, res) => {
+    const { text } = req.body;
+
+    if (text) {
+      db.findById(req.params.id)
+        .then(post => {
+          if (post.length > 0) {
+            db.insertComment({ text: text, post_id: req.params.id })
+              .then(() => {
+                res.status(201).json(text);
+              })
+              .catch(error => {
+                res.status(500).json({ 
+                    error: "There was an error while saving the comment to the database."
+                });
+              });
+          } else {
+            res.status(404).json({ 
+                message: "The post with the specified ID does not exist." 
+            });
+          }
+        })
+        .catch(error => {
+        res.status(500).json({ 
+            error: "Post information could not be retrieved." 
+        });
+        });
+    } else {
+      res.status(400).json({ errorMessage: "Please provide text for the comment." });
+    }
+})
 
 server.listen(8080, () => {
     console.log("\nRunning on port 8080\n")
